5ed75b729cc072706ebc5d11aefdb8cd15a6510a
Diego Yegros <diegoyegrosa@gmail.com>
2023-10-21 19:20:57 -0300

    WIP

--- src/components/Rooms.js
+++ src/components/Rooms.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const Rooms = ({ rooms }) => {
     return (
         <div>
-            <h2 className="mb-4 mt-4">Rooms available</h2>
+            <h2 className="mb-4 mt-4">Available Rooms</h2>
             <div className="d-flex flex-wrap justify-content-start">
                 {rooms.map((roomArray, index) => (
                     <div key={index} className="card m-2 position-relative" style={{width: '18rem'}}>
