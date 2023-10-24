5cc6ec844386744194f6c84228eab782cb959d0f
Diego Yegros <diegoyegrosa@gmail.com>
2023-10-23 22:49:27 -0300

    WIP styling

--- src/components/Rooms.js
+++ src/components/Rooms.js
@@ -7,12 +7,12 @@ const Rooms = ({ rooms }) => {
             <h2 className="mb-4 mt-4">Available Rooms</h2>
             <div className="d-flex flex-wrap justify-content-start">
                 {rooms.map((roomArray, index) => (
-                    <div key={index} className="card m-2 position-relative" style={{width: '18rem'}}>
+                    <div key={index} className="card text-white bg-dark m-2 position-relative" style={{width: '18rem'}}>
                         <div className="card-body">
                             <h5 className="card-title">{roomArray[0].room}</h5>
                         </div>
-                        <div className="card position-absolute bottom-0 end-0">
-                            <small className="text-muted">{roomArray.length}/20</small>
+                        <div className="position-absolute bottom-right">
+                            <small className="white-color">{roomArray.length}/20</small>
                         </div>
                     </div>
                 ))}
