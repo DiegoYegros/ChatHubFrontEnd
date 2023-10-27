87eaec0f9c1ee30871f006b79c307c570abc5fce
Diego Yegros <diegoyegrosa@gmail.com>
2023-10-27 00:50:30 -0300

    allign right dropdown menu

--- src/components/ConnectedUsersDropdown.js
+++ src/components/ConnectedUsersDropdown.js
@@ -1,13 +1,13 @@
 import { Dropdown } from "react-bootstrap";
 const ConnectedUsersDropdown = ({users}) => (
-    <Dropdown className="d-flex justify-content-end">
+    <Dropdown align='end' className="d-flex justify-content-end">
         <Dropdown.Toggle variant="primary">
             {users.length === 1 ? users.length+ " user" : users.length+ " users" }
         </Dropdown.Toggle>
 
-        <Dropdown.Menu>
+        <Dropdown.Menu className="dark-input-menu no-border">
             {users.map((user, index) => (
-                <Dropdown.Item key={index}>{user}</Dropdown.Item>
+                <Dropdown.Item className='dark-input no-border' key={index}>{user}</Dropdown.Item>
             ))}
         </Dropdown.Menu>
     </Dropdown>
